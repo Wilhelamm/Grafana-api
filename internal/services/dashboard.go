d7a188abb367ac720bd4d1fb7bbf215de4fe1eed
bruno.terres <bruno.terres@gmail.com>
2023-04-20 10:35:29 -0300

    list and get methods added

--- internal/services/dashboard.go
+++ internal/services/dashboard.go
@@ -40,3 +40,66 @@ func CreateDashboard(apiKey, apiUrl, jsonFile string) (*models.DashboardResponse
 
 	return &dashboardResp, nil
 }
+
+func ListDashboards(apiKey, apiUrl string) ([]models.Dashboard, error) {
+	client := http.Client{}
+	url := fmt.Sprintf("%s/api/search?type=dash-db", apiUrl)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %s", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response: %s", err)
+	}
+
+	var dashboards []models.Dashboard
+	err = json.Unmarshal(body, &dashboards)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing response: %s", err)
+	}
+
+	return dashboards, nil
+}
+
+
+func GetDashboardInfo(apiKey, apiUrl, dashboardUID string) (*models.Dashboard, error) {
+	client := http.Client{}
+
+	url := fmt.Sprintf("%s/api/dashboards/uid/%s", apiUrl, dashboardUID)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %s", err)
+	}
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response: %s", err)
+	}
+
+	var dashboard models.Dashboard
+	err = json.Unmarshal(body, &dashboard)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing response: %s", err)
+	}
+
+	return &dashboard, nil
+}
